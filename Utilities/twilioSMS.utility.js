eaaa1eb91b571ae1a27014cb68bafe2a0236ac5b
Ajay Yadav <108425249+yadavajay900500@users.noreply.github.com>
2023-03-22 16:03:32 +0530

    Update twilioSMS.utility.js

--- Utilities/twilioSMS.utility.js
+++ Utilities/twilioSMS.utility.js
@@ -1,5 +1,5 @@
-const accountSid = 'AC756b8d69c8c5cdf1897b1dbb7a5a85f0';
-const authToken = '5cff6251e4a9d95206a00d7a935cea5e';
+const accountSid = 'AC7........................';
+const authToken = '5cff............................';
 const client = require('twilio')(accountSid, authToken);
 
 
